b858e88c2b7f2b4f89132ae667fc4dc430689e44
quorSW <nikitikk060314@gmail.com>
2026-02-21 00:20:14 +0300

    Update pay.js

--- api/pay.js
+++ api/pay.js
@@ -1,9 +1,19 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Этот код сработает, когда Палпалыч пришлет уведомление об оплате
-    console.log("Оплата получена:", req.body);
-    
-    // Ответ серверу Палпалыча, что всё ок
+    const data = req.body; // Данные от Палпалыча
+    const botToken = '8240021229:AAHRWYMrJsKABlmWqf8lzWdOfwXeCe64A-8'; // Вставь сюда токен своего бота
+    const myChatId = '5996401983'; // Вставь свой ID (можно узнать у @userinfobot)
+
+    // Отправляем уведомление тебе в телеграм, что пришла оплата
+    await fetch(`https://api.telegram.org{botToken}/sendMessage`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        chat_id: myChatId,
+        text: `💰 Новая оплата!\nСумма: ${data.Amount} ${data.Currency}\nЗаказ №: ${data.OutSum}`
+      })
+    });
+
     res.status(200).send('OK');
   } else {
     res.status(405).send('Method Not Allowed');
