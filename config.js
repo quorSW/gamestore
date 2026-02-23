405a422071d0fd2cb7f46dd95a57ee8d9dca2cbc
quorSW <nikitikk060314@gmail.com>
2026-02-24 00:08:25 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCgwI33BU8sflHjgtpaZSrcrkyglYDnfyk",
+  authDomain: "clicker-8d071.firebaseapp.com",
+  projectId: "clicker-8d071",
+  storageBucket: "clicker-8d071.firebasestorage.app",
+  messagingSenderId: "917780782620",
+  appId: "1:917780782620:web:b46198d163084738689d9"
+};
+
+const FIREBASE_ENABLED = true;
+
